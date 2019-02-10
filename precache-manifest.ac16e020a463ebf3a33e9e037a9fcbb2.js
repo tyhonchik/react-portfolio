7cc73a4304ced4ccd532c142d7d6ca68312308ee
Andrew Ilin <>
2019-02-10 15:30:15 +0300

    Updates

---
+++ precache-manifest.ac16e020a463ebf3a33e9e037a9fcbb2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "bea8a8ff3d29a50bcf3cb1f33c55bc91",
+    "url": "/react-portfolio/static/media/code.bea8a8ff.png"
+  },
+  {
+    "revision": "027ab07476d79c9ea01f",
+    "url": "/react-portfolio/static/css/main.4f5fcaeb.chunk.css"
+  },
+  {
+    "revision": "b3c4ea503d1691d5baf9",
+    "url": "/react-portfolio/static/js/1.b3c4ea50.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "9a8a1f8c8f9860224f449c21a3bd76d4",
+    "url": "/react-portfolio/static/media/roboto-100.9a8a1f8c.woff2"
+  },
+  {
+    "revision": "c0cc9c92fb877993ea6d421c31d33e09",
+    "url": "/react-portfolio/static/media/roboto-300.c0cc9c92.woff2"
+  },
+  {
+    "revision": "ece6673e477b4d7aca12f04dace5ed60",
+    "url": "/react-portfolio/static/media/roboto-400.ece6673e.woff2"
+  },
+  {
+    "revision": "2e17a64836c0412759543f90460d0ed1",
+    "url": "/react-portfolio/static/media/roboto-900.2e17a648.woff2"
+  },
+  {
+    "revision": "9bcef66b2c83e6c22d73b2b9f9cbc6f3",
+    "url": "/react-portfolio/static/media/co1.9bcef66b.png"
+  },
+  {
+    "revision": "027ab07476d79c9ea01f",
+    "url": "/react-portfolio/static/js/main.027ab074.chunk.js"
+  },
+  {
+    "revision": "a28deed9bc4bdbf1ebb6a3efb2a64c00",
+    "url": "/react-portfolio/static/media/crm_1.a28deed9.png"
+  },
+  {
+    "revision": "e10a39af3043a0732514c330c280cb67",
+    "url": "/react-portfolio/static/media/j57vi.e10a39af.png"
+  },
+  {
+    "revision": "12c67c1ea570f137b6352cc484674e56",
+    "url": "/react-portfolio/static/media/lzfoa.12c67c1e.png"
+  },
+  {
+    "revision": "dc451e3dbfd8fdcf88fdb648cfd86a52",
+    "url": "/react-portfolio/static/media/sfy7u.dc451e3d.png"
+  },
+  {
+    "revision": "cdae373b914cec5d2cd0020abba79d15",
+    "url": "/react-portfolio/static/media/uhynw.cdae373b.png"
+  },
+  {
+    "revision": "740845882b0543038da05210d621df18",
+    "url": "/react-portfolio/static/media/whnuc.74084588.png"
+  },
+  {
+    "revision": "49a7416d8464350b6915be97c455c4e2",
+    "url": "/react-portfolio/static/media/ysh1r.49a7416d.png"
+  },
+  {
+    "revision": "ff0ff6638f30b4fec453c12907db9443",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.491a15520209a720b710dd2de826420d.js"
+  "/react-portfolio/precache-manifest.ac16e020a463ebf3a33e9e037a9fcbb2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
